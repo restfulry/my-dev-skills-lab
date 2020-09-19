da8e9bdbb79037a550a1a875abde065f52b392c3
Ryan Lee <ryanlee@RY-MPB-TB.local>
2020-09-19 18:05:55 -0400

    JS functionality, basic css

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const $addSkill = $('#add-skill');
+
+$addSkill.click(function(e) {
+    // console.log(e, this);
+    let $skillInput = $('.skill-input')[0].value;
+    // console.log(inputSkill);
+
+    const addedSkill = $(`
+    <tr>
+        <td>
+            <button id="remove-skill" class="btn btn-xs btn-danger">X</button>
+        </td>
+        <td class="skill">${$skillInput}</td>
+    </tr>
+    `)
+
+    $('table').append(addedSkill);
+});
+
+$('#my-skills-div').on('click','button', function(e){
+    console.log('clicked Remove');
+    $(this).closest('tr').remove();
+})
